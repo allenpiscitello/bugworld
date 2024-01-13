c8cf27fc9d2d2c184196c008a5dfdb446944d631
Allen <allen.piscitello@gmail.com>
2024-01-13 14:18:35 -0600

    start of exercise 1

--- src/cell.rs
+++ src/cell.rs
@@ -1,26 +1,4 @@
-use crate::direction::SquareDirections;
-
-pub trait Cell {}
-
 #[derive(Clone, Copy, PartialEq, Debug)]
-pub struct SquareCell {
-    row: u8,
-    column: u8,
-}
+pub struct SquareCell {}
 
-impl SquareCell {
-    pub fn new(row: u8, column: u8) -> Self {
-        Self { row, column }
-    }
-
-    pub fn get_neighbor_coordinates(&self, direction: &SquareDirections) -> (i32, i32) {
-        match direction {
-            SquareDirections::North => (self.row as i32 - 1, self.column as i32),
-            SquareDirections::South => (self.row as i32 + 1 as i32, self.column as i32),
-            SquareDirections::East => (self.row as i32, self.column as i32 - 1),
-            SquareDirections::West => (self.row as i32, self.column as i32 + 1),
-        }
-    }
-}
-
-impl Cell for SquareCell {}
+impl SquareCell {}
